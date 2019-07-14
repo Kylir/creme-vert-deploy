99163643f9cf23c030bcb098dc75e4cfbd9d525d
Kylir <jason.forest@glyphe.org>
2019-07-14 13:27:15 +0100

    initial commit

---
+++ precache-manifest.25f9017d0e477274248438c24e585163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5887bb76137c7b84129ce1a1bcc98c05",
+    "url": "/creme-verte-deploy/index.html"
+  },
+  {
+    "revision": "1b2835812d60e46c2ca6",
+    "url": "/creme-verte-deploy/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "18fe20af9504d18fb1cc",
+    "url": "/creme-verte-deploy/static/js/2.5f9f68f8.chunk.js"
+  },
+  {
+    "revision": "1b2835812d60e46c2ca6",
+    "url": "/creme-verte-deploy/static/js/main.cddb293c.chunk.js"
+  },
+  {
+    "revision": "6dd55d766f18b379b5b9",
+    "url": "/creme-verte-deploy/static/js/runtime~main.c9a2b10f.js"
+  }
+]);
